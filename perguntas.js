919f36533b4eb9d48dc9e59532436a69d902d186
Luan290Simoes <simoes.luan@escola.pr.gov.br>
2025-08-04 12:17:02 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jesus Cristo',
+    'Quem é Jesus Cristo?',
+    'O messias, o alpha, o omega, o inicio e o fim, o que há de vir e o que veio. O filho de Deus.'
+)
+
+criaCartao(
+    'Evangelho',
+    ' Com quantos anos Jesus morreu?',
+    '33 anos'
+)
+
+criaCartao(
+    'Ressureição',
+    'Quem ressuscitou depois de 3 dias morto?',
+    'Jesus Cristo, o filho de Deus.'
+)
+
+criaCartao(
+    'Vida de Jesus',
+    'Com que idade Jesus começou seu ministerio?',
+    '30 anos.'
+)
